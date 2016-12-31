5350dd83335232a27b8b921091993918c006ebc7
AKA Zippo <2014040035@email.szu.edu.cn>
2016-12-31 15:37:30 -0800

    player should be the last one to draw

--- firstgame/main.py
+++ firstgame/main.py
@@ -19,7 +19,6 @@ while 1:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     # 6 - draw the screen elements
-    screen.blit(player, (100,100))
     for x in range(width/grass.get_width()+1):
         for y in range(height/grass.get_height()+1):
             screen.blit(grass,(x*100,y*100))
@@ -27,6 +26,7 @@ while 1:
     screen.blit(castle,(0,2*(height-castle.get_height()*4)/5 + castle.get_height()))
     screen.blit(castle,(0,3*(height-castle.get_height()*4)/5 + castle.get_height()*2))
     screen.blit(castle,(0,4*(height-castle.get_height()*4)/5 + castle.get_height()*3))
+    screen.blit(player, (100,100))
     # 7 - update the screen
     pygame.display.flip()
     # 8 - loop through the events
