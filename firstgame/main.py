fdc90b89f810259fec1064299d7133c85e99e4ea
AKA Zippo <2014040035@email.szu.edu.cn>
2016-12-31 15:36:30 -0800

    adding background and castle

--- firstgame/main.py
+++ firstgame/main.py
@@ -6,22 +6,27 @@ from pygame.locals import *
  
 # 2 - Initialize the game
 pygame.init()
-width, height = 640, 480
+width, height = 1280, 720
 screen=pygame.display.set_mode((width, height))
-grass = pygame.image.load("resources/images/grass.png")
-castle = pygame.image.load("resources/images/castle.png")
+
 # 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
-screen.blit(castle,(0,(height-420)/5))
-screen.blit(castle,(0,2*(height-420)/5 + 105))
-screen.blit(castle,(0,3*(height-420)/5 + 2*105))
-screen.blit(castle,(0,4*(height-420)/5 + 3*105))
+grass = pygame.image.load("resources/images/grass.png")
+castle = pygame.image.load("resources/images/castle.png")
+
 # 4 - keep looping through
 while 1:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     # 6 - draw the screen elements
     screen.blit(player, (100,100))
+    for x in range(width/grass.get_width()+1):
+        for y in range(height/grass.get_height()+1):
+            screen.blit(grass,(x*100,y*100))
+    screen.blit(castle,(0,(height-castle.get_height()*4)/5))
+    screen.blit(castle,(0,2*(height-castle.get_height()*4)/5 + castle.get_height()))
+    screen.blit(castle,(0,3*(height-castle.get_height()*4)/5 + castle.get_height()*2))
+    screen.blit(castle,(0,4*(height-castle.get_height()*4)/5 + castle.get_height()*3))
     # 7 - update the screen
     pygame.display.flip()
     # 8 - loop through the events
